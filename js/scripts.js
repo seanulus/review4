9c1581cf32ae68bf98b7de419cfe30b0a2355eae
Sean McDemrott <seanjmac@hotmail.com>
2018-03-29 18:12:15 -0700

    add functionality to gather input from check boxes

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,21 @@ function Pizza(size, meat, veggie, price) {
 $(document).ready(function() {
   $("#pizza-order").submit(function(event) {
     event.preventDefault();
+    debugger;
     var size = $("#size").val();
-    var pizza = new Pizza(size, meat, veggie, price);
-
-    $("input:checkbox[name=size]:checked").each(function(){
-      var workTransportationMode = $(this).val();
-      userResponses.push(workTransportationMode);
+    var meats = [];
+    var veggies = [];
+    var price = [];
+    var pizza = new Pizza(size, meats, veggies, price);
+    $("input:checkbox[name=meat-topping]:checked").each(function(){
+      var meat = $(this).val();
+      console.log(meat);
+      meats.push(meat);
     });
-
+    $("input:checkbox[name=veggie-topping]:checked").each(function(){
+      var veggie = $(this).val();
+      console.log(veggie);
+      veggies.push(veggie);
+    });
   });
 });
